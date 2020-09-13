eace6ac76a9844194796fb2a9fef76435903603c
zachary mines <zmines@wesleyan.edu>
2020-09-13 16:23:15 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class App < Sinatra::Base
   end
 
   post '/food' do 
-    "My name is #{params[:name]}, and I love #{params[:favorite_food]}"
+    "My name is #{params[:name]}, and I love #{params[:favorite_food]}."
   end 
 
 end
